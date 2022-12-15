1104a06af0f948b2d3852f215e4a9013b23718af
litepupp <116616252+litepupp@users.noreply.github.com>
2022-12-15 15:05:09 -0600

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,5 @@
 
 int main() {
     std::cout << "Hello World!\n";
-    const char* temp = "qwr";
-            int test = 10;
-    // aijfioaenfoiaenfoaifjar
-     char *foainrfoiaenvoiaen3n2ri249 =    "e";
     return 0;
 }
